450131073668c9080c6d0d7371d191ef13d5045b
Huyson102 <lehuyson1998@gmai.com>
2019-05-21 15:08:33 +0700

    '.'

---
+++ session1/intro.js
@@ -0,0 +1,110 @@
+// let name = 1;
+// name = 5;
+// console.log("Hello", name);
+
+// đây là code cộng 2 số
+// let x = Number(prompt("Nhập số thứ nhất"));
+// let y = Number(prompt("Nhập số thứ hai"));
+// console.log(x + y);
+
+// fd(100)
+// rt(90)
+
+// clear()
+
+// for (let i = 0; i < 3; i++ )
+////////////////////LÀM BÀI TẬP VỀ NHÀ
+// ----------------------------------------------
+
+////////// cách 1
+// let x = Number(prompt("Radius"));
+// console.log("are:", 3.14*Number(x)*Number(x));
+
+////////// cách 2
+// let x = Number(prompt("Nhập bán kính"));
+// let y = 3.14*Number(x)*Number(x);
+// console.log("Diện tích là:", y);
+// ---------------------------------------------
+
+// let y = Number(prompt("Nhập độ C:"));
+// console.log("Độ F là:", 1.8*Number(y) + 32);
+
+
+// ---------------------------------------------
+///////// Hình vuông
+// clear()
+// fd(100)
+// rt(90)
+// fd(100)
+// rt(90)
+// fd(100)
+// rt(90)
+// fd(100)
+// rt(90)
+
+/////////// Cách 2
+// clear()
+// for (let i = 0; i < 2; i++ )
+// {
+// fd(100)
+// rt(90)
+// fd(100)
+
+// rt(90)
+// }
+// --------------------------------------------
+/////////// Hình tam giác
+// clear()
+// rt(30)
+// fd(100)
+// rt(120)
+// fd(100)
+// rt(120)
+// fd(100)
+
+
+// cách 2
+// clear()
+// for (let i = 0; i < 3; i++ )
+// {
+// rt(30)
+// fd(100)
+// rt(90) 
+// } 
+// ---------------------------------------------
+
+////////////// Hình tròn
+// clear()
+// for (let i = 0; i < 36; i++ )
+// {
+// rt(5)
+// fd(10)
+// rt(5) 
+// }
+  
+// ---------------------------------------------
+
+///////////////// Nhiều (6) hình tròn
+
+// clear()
+// for (let j = 0; j < 6; j++)
+// {
+// for (let i = 0; i < 36; i++ ){
+// rt(5)
+// fd(10)
+// rt(5) 
+// }
+// rt(60)}
+  
+// ---------------------------------------------
+
+////////////////// Nhiều hình tròn
+// clear()
+// for (let j = 0; j < 37; j++)
+// {
+// for (let i = 0; i < 36; i++ ){
+// rt(5)
+// fd(10)
+// rt(5) 
+// }
+// rt(10)}

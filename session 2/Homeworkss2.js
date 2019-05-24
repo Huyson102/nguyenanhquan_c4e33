376f57a48ce0ffa6ae9cb11dd37c2ecf320b1399
Huy Sơn <50860972+Huyson102@users.noreply.github.com>
2019-05-24 23:34:52 +0700

    Update Homeworkss2.js

--- session 2/Homeworkss2.js
+++ session 2/Homeworkss2.js
@@ -33,4 +33,68 @@ E3.
                 print("x is greater than y")
             else:
                 print("x and y must be equal")
-                
+
+
+////Turtle exercises
+
+H1.
+	clear()
+	for (let i=0; i<4; i++)
+	{color("red")
+	lt(30)
+	fd(50)
+	rt(60)
+	fd(50)
+	rt(120)
+	fd(50)
+	rt(60)
+	fd(50)
+
+	rt(60)}
+
+H2.
+	clear()
+	let l = 3
+	for(let h = 0; h<4; h++){
+	k = l + h
+	if (k%2 === 0)
+		{color("red")}
+	else {color("blue")}
+	let i = (1-(2/k))*180
+		j = 90-i
+		rt(j)
+	for (let n = 1; n <= k; n++)
+		{fd(75)
+		rt(180-i)}
+		home()}
+// Serious exercises
+
+E1.
+	let h = prompt("Enter your height (cm): ")
+	let w = prompt("Enter your weight (kg): ")
+	let hm = Number (h/100)
+	let bmi = Number (w/(hm*hm))
+	if (bmi<16){
+	    console.log("Severely underweight")
+	}
+	else if (bmi<18.5){
+	    console.log("Underweight")
+	}
+	else if (bmi<25){
+	    console.log("Normal")
+	}
+	else if (bmi<30){
+	    console.log("Overweight")
+	}
+	else {
+	    console.log("Obese")
+	}
+
+E2.
+	var x = prompt("Input x: ");
+	let j = 1;
+	let z = 1
+	for (let z = 1; z <= x; z++){
+		let j = j*z;
+	}
+	console.log(j)

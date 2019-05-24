c91e39f22d2264c6bcac7cf06e81b36484de6d8c
Huyson102 <lehuyson1998@gmai.com>
2019-05-24 15:56:31 +0700

    .

---
+++ session 2/Homeworkss2.js
@@ -0,0 +1,36 @@
+E1.	What is Boolean? 
+        A JavaScript Boolean represents one of two values: true or false.
+
+
+    Write down 3 different expression that results a Boolean type (i.e. 5 == 6)?
+        var a = boolean(10); //returns true
+        var b = boolean(21.5); //returns true
+        var c = boolean("homework"); //returns true
+
+E2. What is a flow chart? 
+        A flowchart is a diagram that depicts a process, system or computer algorithm. 
+ 
+    Draw a flow chart:
+        if name == “Huy be": -----------------> print(“Hand some") //True
+                ||
+                ||//False
+                ||
+                name == “Huy big":------------>even_more_handsome = True (//True)
+                     ||
+                     || //False
+                     ||
+                     webbrowser.open(“https://www.youtube.com/watch?v=04854XqcfCY”) 
+E3. 
+	What is nested conditionals?
+        Nested conditionals is when one conditional is nested inside another conditional.
+
+    Write a piece of code that uses nested conditionals?
+
+        if x < y:
+            print("x is less than y")
+        else:
+            if x > y:
+                print("x is greater than y")
+            else:
+                print("x and y must be equal")
+                
--- session1/intro.js
+++ session1/intro.js
@@ -13,98 +13,132 @@
 // clear()
 
 // for (let i = 0; i < 3; i++ )
-////////////////////LÀM BÀI TẬP VỀ NHÀ
+
+
+
+LÀM BÀI TẬP VỀ NHÀ
 // ----------------------------------------------
+// E1
+// a/
+let 'abc';        // "string"
+let 10;           // "number"
+let true;         // "boolean"
+let {};           // "object"
+let function(){}; // "function"
+let undefined;    // "undefined"
+// b/
+// Tên biến không được bắt đầu bằng số và không chứa các kí tự đặc biệt như &, *, (, ).
+// Tên biến không được trùng với từ khóa
+// Tên biến phải là các chữ không dấu viết  hoa hoặc viết thường, các chữ số từ 0-9 và dấu gạch dưới (_).
 
-////////// cách 1
-// let x = Number(prompt("Radius"));
-// console.log("are:", 3.14*Number(x)*Number(x));
+Let $name; // Sai vì có ký tự đặc biệt $
+Let abc name; // Sai vì có khoảng trắng
+Let name // Sai vì thiếu dấu chấm phẩy ở cuối lệnh
+Let 1name; // Sai vì ký tự đầu tiên là số
+Let name1 name 2 name 3; // sai vì thiếu dấu phẩy ngăn cách giữa các biến
 
-////////// cách 2
+
+//E2
+
+let x = Number(prompt("Radius"));
+console.log("Diện tích là:", 3.14*Number(x)*Number(x));
+
+// ////////// 
 // let x = Number(prompt("Nhập bán kính"));
 // let y = 3.14*Number(x)*Number(x);
 // console.log("Diện tích là:", y);
 // ---------------------------------------------
 
-// let y = Number(prompt("Nhập độ C:"));
-// console.log("Độ F là:", 1.8*Number(y) + 32);
+
+// E3
+let y = Number(prompt("Nhập độ C:"));
+console.log("Độ F là:", 1.8*Number(y) + 32);
 
 
 // ---------------------------------------------
-///////// Hình vuông
-// clear()
-// fd(100)
-// rt(90)
-// fd(100)
-// rt(90)
-// fd(100)
-// rt(90)
-// fd(100)
-// rt(90)
+
+// E4
+//Hình vuông
+clear()
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
+rt(90)
 
 /////////// Cách 2
-// clear()
-// for (let i = 0; i < 2; i++ )
-// {
-// fd(100)
-// rt(90)
-// fd(100)
+clear()
+for (let i = 0; i < 2; i++ )
+{
+fd(100)
+rt(90)
+fd(100)
 
-// rt(90)
-// }
+rt(90)
+}
 // --------------------------------------------
 /////////// Hình tam giác
-// clear()
-// rt(30)
-// fd(100)
-// rt(120)
-// fd(100)
-// rt(120)
-// fd(100)
+clear()
+rt(30)
+fd(100)
+rt(120)
+fd(100)
+rt(120)
+fd(100)
 
 
 // cách 2
-// clear()
-// for (let i = 0; i < 3; i++ )
-// {
-// rt(30)
-// fd(100)
-// rt(90) 
-// } 
+clear()
+for (let i = 0; i < 3; i++ )
+{
+rt(30)
+fd(100)
+rt(90) 
+} 
 // ---------------------------------------------
 
 ////////////// Hình tròn
-// clear()
-// for (let i = 0; i < 36; i++ )
-// {
-// rt(5)
-// fd(10)
-// rt(5) 
-// }
+clear()
+for (let i = 0; i < 36; i++ )
+{
+rt(5)
+fd(10)
+rt(5) 
+}
   
 // ---------------------------------------------
 
-///////////////// Nhiều (6) hình tròn
+///////////////// (6) hình tròn lồng liên tiếp
 
-// clear()
-// for (let j = 0; j < 6; j++)
-// {
-// for (let i = 0; i < 36; i++ ){
-// rt(5)
-// fd(10)
-// rt(5) 
-// }
-// rt(60)}
+clear()
+for (let j = 0; j < 6; j++)
+{
+for (let i = 0; i < 36; i++ ){
+rt(5)
+fd(10)
+rt(5) 
+}
+rt(60)}
   
 // ---------------------------------------------
 
-////////////////// Nhiều hình tròn
-// clear()
-// for (let j = 0; j < 37; j++)
-// {
-// for (let i = 0; i < 36; i++ ){
-// rt(5)
-// fd(10)
-// rt(5) 
-// }
-// rt(10)}
+////////////////// Nhiều hình tròn lồng liên tiếp
+clear()
+for (let j = 0; j < 37; j++)
+{
+for (let i = 0; i < 36; i++ ){
+rt(5)
+fd(10)
+rt(5) 
+}
+rt(10)}
+
+
+
+
+
+
+
